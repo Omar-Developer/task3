be188dbdf638dd736f5c40e4216f24eb80c1ae1b
Omar-Developer <66532063+Omar-Developer@users.noreply.github.com>
2020-06-28 04:36:49 +0300

    hello

---
+++ app/src/main/java/com/omar/controlpanel/MainActivity.java
@@ -0,0 +1,17 @@
+package com.omar.controlpanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://android-b.com/Task1/task.php");
+    }
+}
